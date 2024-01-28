96f0e5b55e9867f6a4da9698ac111a27add6a2f8
koseran <kostassarantavgas2011@gmail.com>
2024-01-28 21:39:00 +0200

    Final Commit

--- src/main/java/org/example/kafkaApplication/Producer/ProducerMain.java
+++ src/main/java/org/example/kafkaApplication/Producer/ProducerMain.java
@@ -1,8 +1,10 @@
 package org.example.kafkaApplication.Producer;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.kafkaApplication.JsonSerializer;
 import org.apache.kafka.clients.producer.*;
+import org.example.kafkaApplication.JsonSerializer;
+
 import java.util.Properties;
 
 public class ProducerMain {
@@ -11,6 +13,10 @@ public class ProducerMain {
         properties.put("bootstrap.servers", "localhost:9092");
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+
+        // Προσθήκη ρυθμίσεων για τα partitions
+        properties.put("partitioner.class", "org.apache.kafka.clients.producer.RoundRobinPartitioner");
+       properties.put("num.partitions", 6);
 
         try (org.apache.kafka.clients.producer.Producer<String, Task> producer = new KafkaProducer<>(properties)) {
             for (int i = 0; i < 20; i++) {
@@ -23,7 +29,9 @@ public class ProducerMain {
                     ObjectMapper obj = new ObjectMapper();
                     String jsonΤask = obj.writeValueAsString(task);
                     System.out.println(jsonΤask);
-                    ProducerRecord<String, Task> record = new ProducerRecord<>("task.events", task);
+
+                    // Χρήση του constructor με partition parameter για να καθορίσετε το partition
+                    ProducerRecord<String, Task> record = new ProducerRecord<>("task.events", i % 6, null, task);
                     producer.send(record);
                 } catch (Exception e) {
                     System.out.println("Problem in producer");
